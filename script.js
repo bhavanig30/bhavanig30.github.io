7a9463acd71e710c7d9381bc8c28c6165eacf974
Bhavani G <bhavanig3003@gmail.com>
2025-03-07 23:55:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Highlight active nav link
+document.querySelectorAll("nav ul li a").forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+    }
+});
